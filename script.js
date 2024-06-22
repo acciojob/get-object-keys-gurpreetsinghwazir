952036e598128a437d5fa94b092724391e76ba9c
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-06-22 11:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let student = {
   name:"gurpreet",
   age:"23",
-  city:"5,7",
-  getKeys() {
-    let arr =[]
-    for(let key in student)
-    arr.push(key)
-    return arr
-  }
+  height:"5,7",
+  weight:"68"
+  
 };
-student.getKeys();
+
+function getKeys() {
+    let arr =[]
+    for(let key in student){
+    arr.push(key)
+    
+}
+console.log(arr)
+  
+}
+
+getKeys(student)
